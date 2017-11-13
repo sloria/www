d4d2a747af86a4685b385c2f081c1e5fcdaaa4fe
Steven Loria <sloria1@gmail.com>
2017-11-12 21:20:03 -0500

    Update requirements

--- tasks.py
+++ tasks.py
@@ -8,36 +8,44 @@ REPO = 'https://github.com/sloria/sloria.github.io.git'
 
 run = partial(inv_run, echo=True)
 
+
 @task
-def clean():
+def clean(ctx):
     if os.path.isdir(DEPLOY_PATH):
         run('rm -rf {deploy_path}'.format(deploy_path=DEPLOY_PATH))
         run('mkdir {deploy_path}'.format(deploy_path=DEPLOY_PATH))
 
+
 @task
-def build():
+def build(ctx):
     run('pelican -s pelicanconf.py')
 
-@task
-def rebuild():
-    clean()
-    build()
 
 @task
-def regenerate():
+def rebuild(ctx):
+    clean(ctx)
+    build(ctx)
+
+
+@task
+def regenerate(ctx):
     run('pelican -r -s pelicanconf.py')
 
-@task
-def serve(port=1234):
-    run('cd {deploy_path} && python -m http.server {port}'.format(deploy_path=DEPLOY_PATH, port=port))
 
 @task
-def reserve():
-    build()
-    serve()
+def serve(ctx, port=1234):
+    run('cd {deploy_path} && python -m http.server {port}'.format(
+        deploy_path=DEPLOY_PATH, port=port))
+
 
 @task
-def publish():
+def reserve(ctx):
+    build(ctx)
+    serve(ctx)
+
+
+@task
+def publish(ctx):
     run('pelican content -o output -s pelicanconf.py')
     run('ghp-import output')
     run('git push {repo}  gh-pages:master'.format(repo=REPO))
