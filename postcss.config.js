3c7e7030a871aaf975bcd96cf1059c89bdeb228c
Steven Loria <sloria1@gmail.com>
2017-12-28 01:16:51 -0500

    Refactor to use media() function to define type scale steps

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,11 @@
-const cssnext = require('postcss-cssnext');
 const cssnano = require('cssnano');
 
 const PROD = process.env.NODE_ENV === 'production';
 
 module.exports = {
   plugins: [
-    cssnext(),
+    require('postcss-cssnext'),
+    require('postcss-media-fn'),
     PROD && cssnano({autoprefixer: false}),
   ],
 };
